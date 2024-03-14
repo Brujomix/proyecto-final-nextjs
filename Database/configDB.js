3c816a6a6ab63783aea85c8fd84ca9bdcdf7e8b8
Trobbiani Bruno <brujomix.bt@gmail.com>
2024-03-14 20:32:12 -0300

    modificamos database

--- Database/configDB.js
+++ Database/configDB.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
