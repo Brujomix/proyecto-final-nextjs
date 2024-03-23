7f68673cdffbd591d02a7279f0c6889bf673d333
Brujomix <brujomix.bt@gmail.com>
2024-03-23 13:03:35 -0300

    seguimos trasladando

--- app/Admin/Reportes/layout.jsx
+++ app/Admin/Reportes/layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SubMenuReportes from "../Components/SubMenus/SubMenuReportes";
+import SubMenuReportes from "../../Components/SubMenus/SubMenuReportes";
 
 function layoutReportes({ children }) {
   return (
