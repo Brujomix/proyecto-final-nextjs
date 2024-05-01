acd85525a0bfd6409536d8fe6c7ef388fefbb27d
Trobbiani Bruno <brujomix.bt@gmail.com>
2024-05-01 18:39:46 -0300

    reemplazamos url

--- app/Utilidades/UrlServer.js
+++ app/Utilidades/UrlServer.js
@@ -1,2 +1,2 @@
-//export const UrlServer_nube = "http://localhost:4000";
-export const UrlServer_nube = "http://200.58.105.75";
+export const UrlServer_nube = "https://dewebdesarrollos.com";
+//export const UrlServer_nube = "http://200.58.105.75";
