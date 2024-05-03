6e735daab80fe41fb762790fffe3d03cb0502f47
Trobbiani Bruno <brujomix.bt@gmail.com>
2024-05-03 20:21:14 -0300

    server incorporado local

--- app/Utilidades/UrlServer.js
+++ app/Utilidades/UrlServer.js
@@ -1,2 +1,3 @@
-export const UrlServer_nube = "https://dewebdesarrollos.com";
+//export const UrlServer_nube = "https://dewebdesarrollos.com";
+export const UrlServer_nube = "http://localhost:400";
 //export const UrlServer_nube = "http://200.58.105.75";
