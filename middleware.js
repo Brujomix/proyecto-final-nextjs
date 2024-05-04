96dd6dc85593e6979fde56e2f146f9626d508017
Trobbiani Bruno <brujomix.bt@gmail.com>
2024-05-03 22:59:00 -0300

    no puedo acceder a la cookie desde midlware

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-   const auth = cookies().get("mi_Cookie_Usuario")
+   const auth = cookies().get("mi_Cookie_Usuario");
    console.log(auth);
   return NextResponse.redirect(new URL('/', request.url))
 }
