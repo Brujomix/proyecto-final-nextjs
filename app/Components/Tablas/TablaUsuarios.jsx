5bba1b3a6c94a3b3601e6fe38fe9fd8e3a4cad7d
Brujomix <brujomix.bt@gmail.com>
2024-05-07 01:21:21 -0300

    estilos tabla usuarios

--- app/Components/Tablas/TablaUsuarios.jsx
+++ app/Components/Tablas/TablaUsuarios.jsx
@@ -11,18 +11,18 @@ export function TablaUsuarios({ Usuarios, Roles }) {
     <table className="w-11/12">
       <thead className="bg-neutral-200">
         <tr className="font-semibold italic tracking-wider">
-          <td className="text-center p-2">Email</td>
-          <td className="text-center p-2">Nombre</td>
-          <td className="text-center p-2">Rol</td>
-          <td className="text-center p-2">Dire</td>
-          <td className="text-center p-2">Tel</td>
-          <td className="text-center p-2">Borrar</td>
+          <td className="text-center text-sm py-2 hidden md:block">Email</td>
+          <td className="text-center text-sm py-2">Nombre</td>
+          <td className="text-center text-sm py-2">Rol</td>
+          <td className="text-center text-sm py-2">Dire</td>
+          <td className="text-center text-sm py-2">Tel</td>
+          <td className="text-center text-sm py-2">Borrar</td>
         </tr>
       </thead>
       <tbody className="text-center">
         {listaUsuarios.map((e) => (
             <tr key={e.us_iden}>
-              <td className="text-center">
+              <td className="text-center hidden md:block">
                 {e.us_email}
               </td>
               <td className="py-2 px-2 text-sm italic">{e.us_name}</td>
