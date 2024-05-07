3b1e0019d3760abfbe4bb9a193b8d9a8f6603322
Brujomix <brujomix.bt@gmail.com>
2024-05-07 00:50:50 -0300

    iconrporamos framer motion

--- app/ui/Carrito_Icon.jsx
+++ app/ui/Carrito_Icon.jsx
@@ -1,20 +1,37 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { FiShoppingCart } from "react-icons/fi";
 import { SumaItemsCarrito } from "@/app/Utilidades/Utils_Carrito";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 export function CarritoIcon() {
-  const itemsCarrito = useSelector(state => state.Carrito.itemsCarrito)
+  const itemsCarrito = useSelector((state) => state.Carrito.itemsCarrito);
+  const [animaCarrito, setAnimaCarrito] = useState(false);
+
+  useEffect(() => {
+    setAnimaCarrito(true);
+    const timer = setTimeout(() => {
+      setAnimaCarrito(false);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [itemsCarrito]);
+
   return (
-    <div>
+    <motion.div
+      animate={{
+        scale: animaCarrito ? 1.2 : 1,
+        opacity: animaCarrito ? 0.3 : 1
+      }}
+      transition={{ duration: 0.3 }}
+    >
       <Link defaultChecked={false} href={"/Carrito"}>
         <FiShoppingCart size={32} />
       </Link>
       <span className="text-sm font-semibold rounded-full bg-white">
         {SumaItemsCarrito(itemsCarrito)}
       </span>
-    </div>
+    </motion.div>
   );
 }
